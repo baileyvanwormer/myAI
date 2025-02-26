d778c7802be741906f6e7fabb6b7a702e0d9cb31
Bailey Van Wormer <89269750+baileyvanwormer@users.noreply.github.com>
2025-02-25 23:17:24 -0500

    Update identity.ts
    tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Ableton`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `Tony`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Generous`;
 export const AI_ROLE: string = `[AI ROLE]`;
