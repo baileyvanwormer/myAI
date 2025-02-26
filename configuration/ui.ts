5b48af177adb4b1ce9296ee5b51604777b749454
Bailey Van Wormer <89269750+baileyvanwormer@users.noreply.github.com>
2025-02-25 23:10:56 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Ableton Virtual Assistant`;
+export const CHAT_HEADER: string = `Ableton AI Assistant`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Bailey Van Wormer`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `Ableton Virtual Assistant`;
+export const PAGE_TITLE: string = `Ableton AI Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
