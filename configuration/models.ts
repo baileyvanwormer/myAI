3cc6903d160995410b197c7ae7bc05906f37720f
Bailey Van Wormer <89269750+baileyvanwormer@users.noreply.github.com>
2025-03-11 09:57:56 -0400

    Update models.ts

--- configuration/models.ts
+++ configuration/models.ts
@@ -1,19 +1,19 @@
 import { ProviderName } from "@/types";
 
-export const INTENTION_MODEL: string = "ft:gpt-4o-2024-08-06:personal:abletonbot2:B9l2VsqH";
+export const INTENTION_MODEL: string = "gpt-4o-mini";
 export const INTENTION_TEMPERATURE: number = 0.7;
 
 export const RANDOM_RESPONSE_PROVIDER: ProviderName = "openai";
-export const RANDOM_RESPONSE_MODEL: string = "ft:gpt-4o-2024-08-06:personal:abletonbot2:B9l2VsqH";
+export const RANDOM_RESPONSE_MODEL: string = "gpt-4o-mini";
 export const RANDOM_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const HOSTILE_RESPONSE_PROVIDER: ProviderName = "openai";
-export const HOSTILE_RESPONSE_MODEL: string = "ft:gpt-4o-2024-08-06:personal:abletonbot2:B9l2VsqH";
+export const HOSTILE_RESPONSE_MODEL: string = "gpt-4o-mini";
 export const HOSTILE_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const QUESTION_RESPONSE_PROVIDER: ProviderName = "openai";
 export const QUESTION_RESPONSE_MODEL: string = "ft:gpt-4o-2024-08-06:personal:abletonbot2:B9l2VsqH";
 export const QUESTION_RESPONSE_TEMPERATURE: number = 0.7;
 
-export const HYDE_MODEL: string = "ft:gpt-4o-2024-08-06:personal:abletonbot2:B9l2VsqH";
+export const HYDE_MODEL: string = "gpt-4o-mini";
 export const HYDE_TEMPERATURE: number = 0.7;
