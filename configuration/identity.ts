c3b04c189e49e1b08e5fce02ad6ad44cc4c1f293
Bailey Van Wormer <89269750+baileyvanwormer@users.noreply.github.com>
2025-03-16 10:31:52 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Ableton Live`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `Music Production Software Created by Ableton AG`;
 
 export const AI_NAME: string = `Tony`;
 export const AI_TONE: string = `Helpful`;
