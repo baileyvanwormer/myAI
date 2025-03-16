7e867c99cd232ebea43f1f63c31a0d1bf8ed0d93
Bailey Van Wormer <89269750+baileyvanwormer@users.noreply.github.com>
2025-03-16 10:51:04 -0400

    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -44,23 +44,21 @@ Respond with the following tone: ${AI_TONE}
 }
 
 export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
-  return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
-
-Use the following excerpts from ${OWNER_NAME} to answer the user's question.
-If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME}. 
-Make sure to always cite all of your sources using their citation numbers [1], [2], etc. 
-
-If an answer is a procedure listed in steps, list each step with a number 1), 2), 3),... for clarity.
-
-Excerpts from ${OWNER_NAME}:
-${context}
-
-Respond with the following tone: ${AI_TONE}
-
-Now respond to the user's message:
-`;
-}
+   return `
+ ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
+ 
+ Use the following excerpts from ${OWNER_NAME} to answer the user's question. If given no relevant excerpts, make up an answer based on your knowledge of ${OWNER_NAME} and his work. Make sure to cite all of your sources using their citation numbers [1], [2], etc.
+ 
+ Excerpts from ${OWNER_NAME}:
+ ${context}
+ 
+ If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+ 
+ Respond with the following tone: ${AI_TONE}
+ 
+ Now respond to the user's message:
+ `;
+ }
 
 export function RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT() {
   return `
