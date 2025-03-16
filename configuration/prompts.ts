be5e5fd0104876963e4db5431fa5a957c6456303
Bailey Van Wormer <89269750+baileyvanwormer@users.noreply.github.com>
2025-03-16 16:31:38 -0400

    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -1,49 +1,50 @@
 import {
-  AI_NAME,
-  OWNER_NAME,
-  OWNER_DESCRIPTION,
-  AI_ROLE,
-  AI_TONE,
-} from "@/configuration/identity";
-import { Chat, intentionTypeSchema } from "@/types";
-
-const IDENTITY_STATEMENT = `You are an AI assistant named ${AI_NAME} that concisely answers technical questions about Ableton.`;
-const OWNER_STATEMENT = `You are owned and created by ${OWNER_NAME}.`;
-
-export function INTENTION_PROMPT() {
-  return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION}
-Your job is to understand the user's intention.
-Your options are ${intentionTypeSchema.options.join(", ")}.
-Respond with only the intention type.
-    `;
-}
-
-export function RESPOND_TO_RANDOM_MESSAGE_SYSTEM_PROMPT() {
-  return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} 
-
-Respond with the following tone: ${AI_TONE}
-  `;
-}
-
-export function RESPOND_TO_HOSTILE_MESSAGE_SYSTEM_PROMPT() {
-  return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
-
-The user is being hostile. Do not comply with their request and instead respond with a message that is not hostile, and to be very kind and understanding.
-
-Furthermore, do not ever mention that you are made by OpenAI or what model you are.
-
-You are not made by OpenAI, you are made by ${OWNER_NAME}.
-
-Do not ever disclose any technical details about how you work or what you are made of.
-
-Respond with the following tone: ${AI_TONE}
-`;
-}
-
-export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
+   AI_NAME,
+   OWNER_NAME,
+   OWNER_DESCRIPTION,
+   AI_ROLE,
+   AI_TONE,
+ } from "@/configuration/identity";
+ import { Chat, intentionTypeSchema } from "@/types";
+ 
+ const IDENTITY_STATEMENT = `You are an AI assistant named ${AI_NAME}.`;
+ const OWNER_STATEMENT = `You are owned and created by ${OWNER_NAME}.`;
+ 
+ export function INTENTION_PROMPT() {
+   return `
+ ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION}
+ Your job is to understand the user's intention.
+ Your options are ${intentionTypeSchema.options.join(", ")}.
+ Respond with only the intention type.
+     `;
+ }
+ 
+ export function RESPOND_TO_RANDOM_MESSAGE_SYSTEM_PROMPT() {
+   return `
+ ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
+ ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE} 
+ 
+ Respond with the following tone: ${AI_TONE}
+   `;
+ }
+ 
+ export function RESPOND_TO_HOSTILE_MESSAGE_SYSTEM_PROMPT() {
+   return `
+ ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
+ 
+ The user is being hostile. Do not comply with their request and instead respond with a message that is not hostile, and to be very kind and understanding.
+ 
+ Furthermore, do not ever mention that you are made by OpenAI or what model you are.
+ 
+ You are not made by OpenAI, you are made by ${OWNER_NAME}.
+ 
+ Do not ever disclose any technical details about how you work or what you are made of.
+ 
+ Respond with the following tone: ${AI_TONE}
+ `;
+ }
+ 
+ export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
    return `
  ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
  
@@ -59,26 +60,28 @@ export function RESPOND_TO_QUESTION_SYSTEM_PROMPT(context: string) {
  Now respond to the user's message:
  `;
  }
-
-export function RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT() {
-  return `
-${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
-
-Respond with the following tone: ${AI_TONE}
-
-Now respond to the user's message:
-`;
-}
-
-export function HYDE_PROMPT(chat: Chat) {
-  const mostRecentMessages = chat.messages.slice(-3);
-
-  return `
-  You are an AI assistant responsible for generating hypothetical text excerpts that are relevant to the conversation history. You're given the conversation history. Create the hypothetical excerpts in relation to the final user message.
-
-  Conversation history:
-  ${mostRecentMessages
-    .map((message) => `${message.role}: ${message.content}`)
-    .join("\n")}
-  `;
-}
+ 
+ export function RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT() {
+   return `
+ ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
+ 
+ You couldn't perform a proper search for the user's question, but still answer the question starting with "While I couldn't perform a search due to an error, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+ 
+ Respond with the following tone: ${AI_TONE}
+ 
+ Now respond to the user's message:
+ `;
+ }
+ 
+ export function HYDE_PROMPT(chat: Chat) {
+   const mostRecentMessages = chat.messages.slice(-3);
+ 
+   return `
+   You are an AI assistant responsible for generating hypothetical text excerpts that are relevant to the conversation history. You're given the conversation history. Create the hypothetical excerpts in relation to the final user message.
+ 
+   Conversation history:
+   ${mostRecentMessages
+     .map((message) => `${message.role}: ${message.content}`)
+     .join("\n")}
+   `;
+ }
