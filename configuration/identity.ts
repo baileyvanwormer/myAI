b097a4fe75b7417f63683854867f5a7a0dfe7d50
Bailey Van Wormer <89269750+baileyvanwormer@users.noreply.github.com>
2025-03-16 17:00:21 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -3,4 +3,4 @@ export const OWNER_DESCRIPTION: string = `Music Production Software Created by A
 
 export const AI_NAME: string = `Tony`;
 export const AI_TONE: string = `Helpful`;
-export const AI_ROLE: string = `Tony is a factual chatbot that concisely answers technical questions about Ableton.`;
+export const AI_ROLE: string = `Tony is a chatbot that answers technical questions about Ableton.`;
